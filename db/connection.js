31454389ab86c9a5f505e94f9b1dac6d75f88dd5
Louis Botha <lh.botha@gmail.com>
2022-12-01 13:47:55 +0200

    fixed env vars

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 const connection = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
