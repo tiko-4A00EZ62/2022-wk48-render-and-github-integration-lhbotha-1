ff446953d9ad66dfd40c69023c8c7d8a8666297f
Louis Botha <lh.botha@gmail.com>
2022-12-01 14:34:50 +0200

    added a frontend

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
+const cors = require('cors');
 const citiesRouter = require('./routes/cities');
 
 const app = express();
+app.use(express.static('frontend/build'));
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
